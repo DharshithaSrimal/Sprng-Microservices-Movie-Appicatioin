20bb95f1d89f1a31da1ac69835abc932bb9c4d2f
DharshithaSrimal <dharshithasrimal@gmail.com>
2022-08-11 12:15:53 +0530

    Create catalog item model

---
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/movie/moviecatalogservice/models/CatalogItem.java
@@ -0,0 +1,34 @@
+package com.movie.moviecatalogservice.models;
+
+public class CatalogItem {
+    private String name;
+    private String description;
+    private int rating;
+
+    public CatalogItem(String name, String description, int rating) {
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    
+}
