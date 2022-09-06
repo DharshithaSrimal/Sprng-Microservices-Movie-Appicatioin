d229e20f258a46e32cd3bced96f8e6dd1184cb98
DharshithaSrimal <dharshithasrimal@gmail.com>
2022-09-06 06:11:20 +0530

    Create Movie Ratings resource

---
+++ ratings-data-service/ratings-data-service/src/main/java/com/movie/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,11 @@
+package com.movie.ratingsdataservice.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    
+}
