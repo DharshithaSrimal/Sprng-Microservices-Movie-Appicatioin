c5a9e1274d6913d49e6f45c34fdeb92c41832ab8
DharshithaSrimal <dharshithasrimal@gmail.com>
2022-09-06 06:13:15 +0530

    Update RatingsResource.java

--- ratings-data-service/ratings-data-service/src/main/java/com/movie/ratingsdataservice/resources/RatingsResource.java
+++ ratings-data-service/ratings-data-service/src/main/java/com/movie/ratingsdataservice/resources/RatingsResource.java
@@ -2,10 +2,15 @@ package com.movie.ratingsdataservice.resources;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.movie.ratingsdataservice.model.Rating;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
 
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId, 4);
+    }
     
 }
